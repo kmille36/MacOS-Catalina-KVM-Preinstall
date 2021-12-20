47105b77440a5e7cbd1a9e274a11eb21e8490aa9
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-12-20 20:29:24 +0700

    Update MacOS-Mojave.sh

--- MacOS-Mojave.sh
+++ MacOS-Mojave.sh
@@ -8,9 +8,9 @@ sudo apt install git qemu-system qemu-utils python3 python3-pip -y
 git clone https://github.com/foxlet/macOS-Simple-KVM.git
 cd macOS-Simple-KVM
 sudo python3  -m pip install click
-./jumpstart.sh --mojave
-echo "Downloading Mac OS Mojave Disk..." 
-wget -q --show-progress --no-check-certificate -O MyDisk.qcow2 https://tinyurl.com/9v4p8d5c
+./jumpstart.sh --catalina
+echo "Downloading Mac OS Catalina Disk..." 
+wget -q --show-progress --no-check-certificate -O MyDisk.qcow2 https://tinyurl.com/3s87mjea
 cpus=$(lscpu | grep CPU\(s\) | head -1 | cut -f2 -d":" | awk '{$1=$1;print}')
 availableRAMcommand="free -m | tail -2 | head -1 | awk '{print \$7}'"
 availableRAM=$(echo $availableRAMcommand | bash)
@@ -20,5 +20,5 @@ nohup ./silence.sh &>/dev/null &
 echo "All Done !!! Your Mac OS VM now Ready to use !!!"
 echo "Open VNC Viewer and connect with port 5900"
 echo "Your VM Specs: $availableRAM"M" / $cpus CPUs"
-echo "User: Mojave-User"
+echo "User: Catalina-User"
 echo "Pass: 123456"
