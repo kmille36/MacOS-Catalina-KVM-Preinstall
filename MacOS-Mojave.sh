6a545092a7454c2e44959761c17af7518f24a38b
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-12-20 20:27:00 +0700

    Add files via upload

---
+++ MacOS-Mojave.sh
@@ -0,0 +1,24 @@
+echo "Checking Virtualization..."
+vtx=$(egrep -i '^flags.*(vmx|svm)' /proc/cpuinfo | wc -l)
+if [ $vtx = 0 ] ; then echo "[Error] Your PC Not Support VT-x / AMD-v. Press Ctrl+C to quit"&& sleep 2711
+fi
+echo Installing Software...
+sudo apt update -y
+sudo apt install git qemu-system qemu-utils python3 python3-pip -y 
+git clone https://github.com/foxlet/macOS-Simple-KVM.git
+cd macOS-Simple-KVM
+sudo python3  -m pip install click
+./jumpstart.sh --mojave
+echo "Downloading Mac OS Mojave Disk..." 
+wget -q --show-progress --no-check-certificate -O MyDisk.qcow2 https://tinyurl.com/9v4p8d5c
+cpus=$(lscpu | grep CPU\(s\) | head -1 | cut -f2 -d":" | awk '{$1=$1;print}')
+availableRAMcommand="free -m | tail -2 | head -1 | awk '{print \$7}'"
+availableRAM=$(echo $availableRAMcommand | bash)
+echo HEADLESS=1 MEM=$availableRAM"M" CPUS=$cpus SYSTEM_DISK=MyDisk.qcow2 ./headless.sh > silence.sh
+chmod +x silence.sh
+nohup ./silence.sh &>/dev/null &
+echo "All Done !!! Your Mac OS VM now Ready to use !!!"
+echo "Open VNC Viewer and connect with port 5900"
+echo "Your VM Specs: $availableRAM"M" / $cpus CPUs"
+echo "User: Mojave-User"
+echo "Pass: 123456"
---
+++ README.md
@@ -0,0 +1,34 @@
+# MacOS-Mojave-KVM-Preinstall
+Mac OS running on QEMU-KVM (Pre-install 20GB)
+
+-Note: It will use all available free ram and CPUs. The script is only for Ubuntu, Debian.
+
+-How it works: Pre-install QCOW2 (128 GB disk space), connect with VNC at port 5900 
+
+![image](https://user-images.githubusercontent.com/58414694/146664162-a2b95463-207f-4067-a257-227e74fe53db.png)
+
+![image](https://user-images.githubusercontent.com/58414694/146664194-f9c83144-d359-4824-a462-6fdbc6c894ac.png)
+
+![image](https://user-images.githubusercontent.com/58414694/146664225-952c65ea-c676-4c50-a414-58ed3d915c77.png)
+
+![image](https://user-images.githubusercontent.com/58414694/146664401-4a3e782e-ce46-456f-b480-abf105ad0ee6.png)
+
+
+
+Download:
+```console 
+curl -skLo MacOS-Mojave.sh https://bit.ly/3e3KjnN && chmod +x MacOS-Mojave.sh
+```
+
+Usage: 
+- Download and install:
+```console 
+bash MacOS-Mojave.sh 
+```
+- Start MacOS VM in silent:
+
+(By default VM automatic starts after installation is done! Use this when your VM turn off and need to start again)
+```console 
+nohup ./silence.sh &>/dev/null &
+```
+
